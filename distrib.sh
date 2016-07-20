6a9d85d07d4f79a977ffd0d648d25e7f8118777e
Mike Christopher <mchristopher@users.noreply.github.com>
2016-07-20 12:29:36 -0700

    Compress symlinks on OSX and Linux

--- distrib.sh
+++ distrib.sh
@@ -5,7 +5,7 @@ find . -name "*.pyc" -delete
 rm -fr PokemonGoMap-OSX.zip
 node_modules/.bin/electron-packager . "Pokemon GO Live Map" --platform=darwin --arch=x64 --icon=pokemon.icns --overwrite --prune --osx-sign.identity="Mac Developer: Mike Christopher (35938FGD4K)" --ignore=PokemonGoMap-Lin-x64.zip --ignore=PokemonGoMap-OSX.zip --ignore=PokemonGoMap-Win.zip --ignore=map/ve --ignore="map/Easy Setup" --ignore=distrib.sh --ignore=pywin --ignore=package_python.sh
 cd "Pokemon GO Live Map-darwin-x64"
-zip -9rv ../PokemonGoMap-OSX.zip .
+zip -9ryv ../PokemonGoMap-OSX.zip .
 cd ..
 
 rm -fr PokemonGoMap-Win.zip
@@ -17,5 +17,5 @@ cd ..
 rm -fr PokemonGoMap-Lin-x64.zip
 node_modules/.bin/electron-packager . "Pokemon GO Live Map" --platform=linux --arch=x64 --icon=pokemon.ico --overwrite --prune --ignore=map/ve --ignore=PokemonGoMap-Lin-x64.zip --ignore=PokemonGoMap-OSX.zip --ignore=PokemonGoMap-Win.zip --ignore="map/Easy Setup" --ignore=distrib.sh --ignore=package_python.sh --ignore=pywin
 cd "Pokemon GO Live Map-linux-x64"
-zip -9rv ../PokemonGoMap-Lin-x64.zip .
+zip -9ryv ../PokemonGoMap-Lin-x64.zip .
 cd ..
