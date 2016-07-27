f4a7cecf416966f6bd28f8de1d01d64f2cefa208
Mike Christopher <mchristopher@users.noreply.github.com>
2016-07-26 19:34:00 -0700

    Switching to ngrok, updating maps

--- distrib.sh
+++ distrib.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 find . -name "*.pyc" -delete
+rm -fr map/pogom.db
 
 rm -fr PokemonGoMap-OSX.zip
 node_modules/.bin/electron-packager . "Pokemon GO Live Map" --platform=darwin --arch=x64 --icon=pokemon.icns --overwrite --prune --osx-sign.identity="Mac Developer: Mike Christopher (35938FGD4K)" --ignore=PokemonGoMap-Lin-x64.zip --ignore=PokemonGoMap-OSX.zip --ignore=PokemonGoMap-Win.zip --ignore=map/ve --ignore="map/Easy Setup" --ignore=distrib.sh --ignore=pywin --ignore=package_python.sh
